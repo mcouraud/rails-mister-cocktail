8995d0f85e39d37ddca5bec8e8daaf7df4b7eca8
Mathilde Couraud <mcouraud@outlook.fr>
2018-11-22 16:29:06 +0100

    Done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,6 +6,7 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(doses_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
